9b414b6592f68cdf12893a5fa1267dc18e1cc94b
liangzhuang <liang05083700@163.com>
2020-05-18 09:17:29 +0800

    添加升级文档

--- CHANGELOG.zh-CN.md
+++ CHANGELOG.zh-CN.md
@@ -2,6 +2,56 @@
 
 ### 0.0.1
 
-*2020-04-13*
+_2020-05-18_
 
 #### 新特性
+
+1. Convert 转换码信息添加
+
+/\*_ 获取五笔码 _/ wbConvert(text: string): string;
+
+/\*_ 获取全拼码 _/ fullConvert(text: string): string;
+
+/\*_ 获取简拼码 _/ SimpConvert(text: string): string;
+
+### 使用方法
+
+this.\$convert.SimpConvert('汉字') =>
+
+import { Convert } from 'msun-lib-ui';
+
+Convert.SimpConvert('汉字');
+
+2. http 请求约束添加
+
+/\*_ GET _/ 
+$get(url: string, success?: Function, error?: Function): Promise<any>;
+
+/\*_ POST _/ 
+$post(url: string, params: any, success?: Function, error?: Function): Promise<any>;
+
+/\*_ PUT _/ 
+$put(url: string, params: any, success: Function, error: Function): Promise<any>;
+
+/\*_ DELETE _/ 
+$delete(url: string, success?: Function, error?: Function): Promise<any>;
+
+/\*_ REQUEST _/ 
+$request(config?: AxiosRequestConfig, success?: Function, error?: Function): Promise<any>;
+
+/\*_ HEAD _/ 
+$head(url: string, success?: Function, error?: Function, config?: AxiosRequestConfig): Promise<any>;
+
+/\*_ OPTIONS _/ 
+$options(url: string, success?: Function, error?: Function, config?: AxiosRequestConfig): Promise<any>;
+
+/\*_ PATCH _/ 
+$patch(url: string, param: any, success?: Function, error?: Function, config?: AxiosRequestConfig): Promise<any>;
+
+### 使用方法
+
+this.$http.$get() => 
+
+import { Http } from 'msun-lib-ui';
+
+Http.$get()
--- types/http.d.ts
+++ types/http.d.ts
@@ -1,60 +1,29 @@
 import { AxiosRequestConfig } from 'axios';
 
 export interface MsunHttpClient {
-  
   /** GET */
   $get(url: string, success?: Function, error?: Function): Promise<any>;
 
   /** POST */
-  $post(
-    url: string,
-    params: any,
-    success?: Function,
-    error?: Function
-  ): Promise<any>;
+  $post(url: string, params: any, success?: Function, error?: Function): Promise<any>;
 
   /** PUT */
-  $put(
-    url: string,
-    params: any,
-    success: Function,
-    error: Function
-  ): Promise<any>;
+  $put(url: string, params: any, success: Function, error: Function): Promise<any>;
 
   /** DELETE */
   $delete(url: string, success?: Function, error?: Function): Promise<any>;
 
   /** REQUEST */
-  $request(
-    config?: AxiosRequestConfig,
-    success?: Function,
-    error?: Function
-  ): Promise<any>;
+  $request(config?: AxiosRequestConfig, success?: Function, error?: Function): Promise<any>;
 
   /** HEAD */
-  $head(
-    url: string,
-    success?: Function,
-    error?: Function,
-    config?: AxiosRequestConfig
-  ): Promise<any>;
+  $head(url: string, success?: Function, error?: Function, config?: AxiosRequestConfig): Promise<any>;
 
   /** OPTIONS */
-  $options(
-    url: string,
-    success?: Function,
-    error?: Function,
-    config?: AxiosRequestConfig
-  ): Promise<any>;
+  $options(url: string, success?: Function, error?: Function, config?: AxiosRequestConfig): Promise<any>;
 
   /** PATCH */
-  $patch(
-    url: string,
-    param: any,
-    success?: Function,
-    error?: Function,
-    config?: AxiosRequestConfig
-  ): Promise<any>;
+  $patch(url: string, param: any, success?: Function, error?: Function, config?: AxiosRequestConfig): Promise<any>;
 }
 
 declare module 'vue/types/vue' {
