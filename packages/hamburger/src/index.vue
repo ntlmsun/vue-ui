4429ae29cda6565f5594d09532777949ee8721e6
liangzhuang <liang05083700@163.com>
2020-05-25 19:36:18 +0800

    icons update

---
+++ packages/hamburger/src/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+<script>
+export default {
+  name: 'MsunHamburger',
+  props: {
+    isActive: {
+      type: Boolean,
+      default: false,
+      required: true
+    }
+  }
+};
+</script>
